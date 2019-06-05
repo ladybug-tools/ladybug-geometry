04834ae69c771f6ecbade82f5472d8b3c0d9a0b9
Mostapha <sadeghipour@gmail.com>
2019-06-05 09:13:10 -0400

    fix(deploy): use semantic release for deployment to PyPI
    The version for PyPI was set to the hard-coded version in \__init__.py. This is now fixed.

--- setup.py
+++ setup.py
@@ -1,33 +1,19 @@
-import re
 import setuptools
-import sys
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-with open('ladybug_geometry/__init__.py', 'r') as fd:
-    version = re.search(
-        r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]',
-        fd.read(),
-        re.MULTILINE
-    ).group(1)
-
-try:
-    from semantic_release import setup_hook
-    setup_hook(sys.argv)
-except ImportError:
-    pass
-
 setuptools.setup(
     name="ladybug-geometry",
-    version=version,
+    use_scm_version = True,
+    setup_requires=['setuptools_scm'],
     author="Ladybug Tools",
     author_email="info@ladybug.tools",
     description="Ladybug geometry is a Python library that adds geometry objects and basic geometric computation to Ladybug.",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/ladybug-tools/ladybug-geometry",
-    packages=setuptools.find_packages(),
+    packages=setuptools.find_packages(exclude=["tests"]),
     install_requires=[
     ],
     classifiers=[
