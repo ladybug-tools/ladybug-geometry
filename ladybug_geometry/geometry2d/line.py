f36addac1447cdf854288a61430603f9ede047ba
Chris <chris@ladybug.tools>
2019-11-01 18:29:21 -0400

    fix(line): Fix bug in LineSegment.subdivide_evenly method
    This commit fixes a Python tolerance issue that can sometimes arise in the LineSegment.subdivide_evenly method. I was only able to find this one after testing it on several thousand line segments in Rhino. Glad that this bug is squashed!

--- ladybug_geometry/geometry2d/line.py
+++ ladybug_geometry/geometry2d/line.py
@@ -142,13 +142,14 @@ class LineSegment2D(Base1DIn2D):
         Args:
             number: The number of segments into which the line will be divided.
         """
-        interval = 1 / number
+        interval = 1 / number  # can create tolerance issue where interval * number < 1
         parameter = interval
         sub_pts = [self.p]
         while parameter < 1:
             sub_pts.append(self.point_at(parameter))
             parameter += interval
-        sub_pts.append(self.p2)
+        if len(sub_pts) != number + 1:  # avoid tolerance issue from interval division
+            sub_pts.append(self.p2)
         return sub_pts
 
     def point_at(self, parameter):
