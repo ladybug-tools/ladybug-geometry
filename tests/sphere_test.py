4626b5c3b23114e68b0c142fdb8ffc872c0b6e09
sg <sgaray1970@gmail.com>
2019-11-12 11:49:25 -0500

    style(tests): Adding a few additional tests

--- tests/sphere_test.py
+++ tests/sphere_test.py
@@ -2,41 +2,50 @@
 # import pytest
 
 from ladybug_geometry.geometry3d.pointvector import Point3D, Vector3D
-from ladybug_geometry.geometry3d.line import LineSegment3D
-from ladybug_geometry.geometry3d.sphere import Sphere
+from ladybug_geometry.geometry3d.sphere import Sphere3D
 from ladybug_geometry.geometry3d.plane import Plane
+from ladybug_geometry.geometry3d.line import LineSegment3D
+from ladybug_geometry.geometry3d.arc import Arc3D
 
 
 def test_sphere_init():
-    """Test the initalization of Sphere objects and basic properties."""
+    """Test the initalization of Sphere3D objects and basic properties."""
     pt = Point3D(2, 0, 2)
     r = 3
-    sp = Sphere(pt, r)
+    sp = Sphere3D(pt, r)
     str(sp)  # test the string representation of the line segment
 
     assert sp.center == Point3D(2, 0, 2)
     assert sp.radius == 3
+    assert sp.to_dict()['center'] == (2, 0, 2)
 
 
 def test_sphere_intersection_with_line_ray():
-    """Test the Sphere intersect_line_ray method."""
-    lpt = Point3D(1.5, 0, -3)
-    vec = Vector3D(0, 0, 8)
+    """Test the Sphere3D intersect_line_ray method."""
+    lpt = Point3D(-2, 0, 0)
+    vec = Vector3D(4, 0, 0)
     seg = LineSegment3D(lpt, vec)
     spt = Point3D(0, 0, 0)
-    sp = Sphere(spt, 1.5)
+    sp = Sphere3D(spt, 1.5)
 
     int1 = sp.intersect_line_ray(seg)
-    assert len(int1) == 1, "{}".format(int1)
+    assert isinstance(int1, LineSegment3D)
+    assert int1.p == Point3D(1.5, 0, 0)
 
 
 def test_sphere_intersection_with_plane():
-    """Test the Sphere intersect_plane method."""
+    """Test the Sphere3D intersect_plane method."""
     ppt = Point3D(-1.5, 0, 1.46)
     vec = Vector3D(0.1, 0, 1)
-    pl = Plane(n=vec, o=ppt)
+    pl = Plane(vec, ppt)
     spt = Point3D(0, 0, 0)
-    sp = Sphere(spt, 1.5)
-
+    sp = Sphere3D(spt, 1.5)
     int1 = sp.intersect_plane(pl)
-    # assert False, int1
+    assert isinstance(int1, Arc3D)
+
+    ppt = Point3D(0, 0, 0)
+    vec = Vector3D(0, 0, 1)
+    pl = Plane(vec, ppt)
+    int1 = sp.intersect_plane(pl)
+    assert int1.c == ppt
+    assert int1.radius == 1.5
