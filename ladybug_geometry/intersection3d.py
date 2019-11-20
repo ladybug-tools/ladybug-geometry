7792f85b3a39ae5cb6d2dacecc1ac9fd6dae767f
Santiago Garay <sgaray1970@gmail.com>
2019-11-19 19:56:51 -0500

    feat(sphere): Add Sphere3D
    * feat(sphere): Adding Sphere object
    
    Initial class with basic functionality.
    
    * feat(sphere): Adding properties and intersection methods
    
    * fix(sphere_test): Eliminating a forced test fail
    
    * fix(Sphere.intersect_plane): Fixed incorrect math for intersection circle
    
    * feat(Point3D): Adding to_array from_array methods
    
    * style(tests): Adding a few additional tests
    
    * fix(Sphere3D): Fixing several Sphere3D issues
    
    - Fixing some code style issues
    - Fixing several documentation issues
    - Separating the math of the intersection functions out of the Sphere3D class as an intermeiate step before moving them to the intersection3D class.
    
    * fix(Spehre3D): Moving intersection math to intersection3D module (pytest error)
    
    pytest error collecting modules.
    
    * style(tests): Adding a few additional tests
    
    * fix(Sphere3D): Misc. fixes.
    
    - Changing docstring typos
    -Adding missing transformation functions
    - Fixed intersection methods.
    
    * fix(intersection3D): Changing Docstring descriptions and typos
    
    * style(tests): Adding Sphere3D test coverage
    
    * fix(copy): Fix Sphere3D copy function
    
    * fix(tests): Fixing Sphere3D tests
    
    - Renaming test file to sphere3d_test
    - Separating Sphere3D tests by functions
    - Adding additional tests

--- ladybug_geometry/intersection3d.py
+++ ladybug_geometry/intersection3d.py
@@ -4,6 +4,8 @@ from __future__ import division
 
 from .geometry3d.pointvector import Point3D
 
+import math
+
 
 def intersect_line3d_plane(line_ray, plane):
     """Get the intersection between a Ray3D/LineSegment3D and a Plane.
@@ -124,8 +126,8 @@ def closest_point3d_between_line3d_plane(line_ray, plane):
     Returns:
         Two Point3D objects representing
 
-        1) The point on the line_ray that is closest to the plane
-        2) The point on the plane that is closest to the line_ray
+        1) The point on the line_ray that is closest to the plane.
+        2) The point on the plane that is closest to the line_ray.
 
         Will be None if there is an intersection between line_ray and the plane
     """
@@ -140,3 +142,78 @@ def closest_point3d_between_line3d_plane(line_ray, plane):
                            line_ray.p.z + u * line_ray.v.z)
         return close_pt, closest_point3d_on_plane(close_pt, plane)
     return None  # intersection
+
+
+def intersect_line3d_sphere3d(line_ray, sphere):
+    """Get the intersection between this Sphere3D object and a Ray2D/LineSegment2D.
+
+    Args:
+        line_ray: A LineSegment3D or Ray3D for intersection.
+        sphere: A Sphere3D to intersect.
+
+    Returns:
+        Two Point3D objects if a full intersection exists.
+        A Point3D if a point of tangentcy exists.
+        Will be None if no intersection exists.
+
+    """
+    L = line_ray
+    S = sphere
+    a = L.v.magnitude_squared
+    b = 2 * (L.v.x * (L.p.x - S.center.x) +
+             L.v.y * (L.p.y - S.center.y) +
+             L.v.z * (L.p.z - S.center.z))
+    c = S.center.magnitude_squared + \
+        L.p.magnitude_squared - \
+        2 * S.center.dot(L.p) - \
+        S.radius ** 2
+    det = b ** 2 - 4 * a * c
+    if det < 0:
+        return None
+    sq = math.sqrt(det)
+    u1 = (-b + sq) / (2 * a)
+    u2 = (-b - sq) / (2 * a)
+    if not L._u_in(u1):
+        u1 = max(min(u1, 1.0), 0.0)
+    if not L._u_in(u2):
+        u2 = max(min(u2, 1.0), 0.0)
+    p1 = Point3D(L.p.x + u1 * L.v.x, L.p.y + u1 * L.v.y, L.p.z + u1 * L.v.z)
+    if u1 == u2:
+        return p1
+    else:
+        p2 = Point3D(L.p.x + u2 * L.v.x, L.p.y + u2 * L.v.y, L.p.z + u2 * L.v.z)
+        return p1, p2
+
+
+def intersect_plane_sphere3d(plane, sphere):
+    """Get the intersection of a plane with this Sphere3D object
+
+    Args:
+        plane: A Plane object.
+        sphere: A Sphere3D to intersect.
+
+    Returns:
+        If a full intersection exists
+
+        1) A Point3D that represents the center of the intersection circle.
+        2) A Vector3D that represents the normal of the intersection cirlce.
+        3) A number that represents the radius of the intersection circle.
+
+        A Point3D Object if a point of tangentcy exists.
+        None if no intersection exists.
+    """
+    r = sphere.radius
+    pt_c = sphere.center
+    pt_o = plane.o
+    v_n = plane.n.normalize()
+
+    # Resulting circle radius. Radius² = r² - [(c-p).n]²
+    d = (pt_o - pt_c).dot(v_n)
+    if abs(r) < abs(d):  # No intersection if (r ** 2 - d ** 2) negative
+        return None
+    cut_r = math.sqrt(r ** 2 - d ** 2)
+
+    # Intersection circle center point. Center_point = p - [(c-p).n]n
+    cut_center = pt_c + (d * v_n)
+
+    return (cut_center, v_n, cut_r) if cut_r != 0 else cut_center
